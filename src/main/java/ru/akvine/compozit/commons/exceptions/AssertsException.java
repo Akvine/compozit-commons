e351a4f941a93e63f642dd9802e3ea4670fe675f
akvine <redgooseman55@gmail.com>
2025-06-24 18:04:51 +0300

    change Asserts classes function and delete AssertsException

--- src/main/java/ru/akvine/compozit/commons/exceptions/AssertsException.java
+++
@@ -1,7 +0,0 @@
-package ru.akvine.compozit.commons.exceptions;
-
-public class AssertsException extends RuntimeException {
-    public AssertsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/akvine/compozit/commons/utils/Asserts.java
+++ src/main/java/ru/akvine/compozit/commons/utils/Asserts.java
@@ -2,26 +2,67 @@ package ru.akvine.compozit.commons.utils;
 
 import lombok.experimental.UtilityClass;
 import org.apache.commons.lang3.StringUtils;
-import ru.akvine.compozit.commons.exceptions.AssertsException;
 
+import java.util.Collection;
+import java.util.Map;
 import java.util.Objects;
 
 @UtilityClass
 public class Asserts {
-    public void isNotNull(Object object, String message) {
-        if (Objects.isNull(object)) {
-            throw new AssertsException(message);
-        }
-    }
-
     public void isNotNull(Object object) {
         String message = object.getClass().getSimpleName() + " is null!";
         isNotNull(object, message);
     }
 
+    public void isNotNull(Object object, String message) {
+        isNotNull(object, new IllegalArgumentException(message));
+    }
+
+    public <T extends RuntimeException> void isNotNull(Object object, T exception) {
+        if (Objects.isNull(object)) {
+            throw exception;
+        }
+    }
+
+    public void isNotBlank(String content) {
+        isNotBlank(content, "content is blank or null!");
+    }
+
     public void isNotBlank(String content, String message) {
+        isNotBlank(content, new IllegalArgumentException(message));
+    }
+
+    public <T extends RuntimeException> void isNotBlank(String content, T exception) {
         if (StringUtils.isBlank(content)) {
-            throw new AssertsException(message);
+            throw exception;
+        }
+    }
+
+    public void isNotEmpty(Collection<?> collection) {
+        isNotEmpty(collection, "collection is empty or null!");
+    }
+
+    public void isNotEmpty(Collection<?> collection, String message) {
+        isNotEmpty(collection, new IllegalArgumentException(message));
+    }
+
+    public <T extends RuntimeException> void isNotEmpty(Collection<?> collection, T exception) {
+        if (collection == null || collection.isEmpty()) {
+            throw exception;
+        }
+    }
+
+    public void isNotEmpty(Map<?, ?> map) {
+        isNotEmpty(map, "map is empty or null!");
+    }
+
+    public void isNotEmpty(Map<?, ?> map, String message) {
+        isNotEmpty(map, new IllegalArgumentException(message));
+    }
+
+    public <T extends RuntimeException> void isNotEmpty(Map<?, ?> map, T exception) {
+        if (map == null || map.isEmpty()) {
+            throw exception;
         }
     }
 }
